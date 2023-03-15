f963c0843c7950bd394aa4ebb9d540759ec30cf4
Daniel Prochazka <danielprochazka@gmail.com>
2023-03-15 01:02:39 +0100

    Epptec Algorithm task

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # hiring-task-epptec
-Algorithm for the admission procedure.
+
+## Zadání
+Algoritmus\
+Napište algoritmus, který ze seznamu vytřídí prvky podle nějakých pravidel,
+včetně příkladu takového pravidla. Tzn. ať je tam sekce, kam se doplní konkrétní pravidla,
+prvky budou někde mimo v seznamu, který algoritmus projde a smaže hodnoty, které neprošly pravidly.
+Prosíme uvést následně instrukci jak spustit a kde najdeme algoritmus.
+
+## Řešení
+
+### Specifikace vstupních dat:
+
+Jméno zaměstnance, tým, odpracované hodiny (za měsíc), odpracované hodiny na klientských projektech (za měsíc), email,
+dvoufaktorová autentizace povolena.
+
+### Případová studie:
+
+a) Chceme vymazat uživatele, kteří mají větší než 50% utilizaci, aby jsme sledovali efektivitu jednotlivců.
+
+b) Dále nás zajímá, kteří uživatelé používají dvoufaktorovou autentizaci, abychom zajistili větší bezpečnost interního systému.
+
+### Implementace:
+
+Průchod neseřazeným polem, kde při nalezení neodpovídajícího elementu, element odstraníme a pokračujeme v iteraci. Program příjímá první
+argument typu boolean a druhý argument značící pravidlo. Pokud je první argument nastavený na 'True' tak se vstupní soubor modifikuje,
+dle příslušných pravidel. Pokud je nastavený na 'False' tak pouze vypíše na standartní výstup. Druhý argument určuje pravidlo k selekci.
+
+### Dostupné možnosti spuštění:
+
+python main.py True utilization - vymaže všechny uživatele ze vstupního souboru, kteří mají utilizaci vyšší než 50%\
+python main.py True 2fa - vymaže všechny uživatele ze vstupního souboru, kteří mají povolené dvoufázové ověření\
+python main.py False utilization - vypíše všechny uživatele, kteří mají utilizaci nižší než 50%\
+python main.py False 2fa - vypíše všechny uživatele, kteří nemají povolené dvoufázové ověření
---
+++ main.py
@@ -0,0 +1,87 @@
+import csv
+import sys
+import os
+
+users_database = 'user_database.csv' # Define the name of the users database file
+two_fa ='twofactor_authentication_enabled' # Define the column name for the two-factor authentication column
+
+# Define a function to filter users based on utilization
+def utilization_rule(user):
+    utilization = float(user['worked_hours_on_project_per_month']) / float(user['worked_hours_per_moth'])
+    return utilization < 0.5
+
+# Define a function to check if two-factor authentication is enabled
+def two_factor_enabled(user):
+    return user[two_fa] == 'True'
+
+# Define a function to check if two-factor authentication is disabled
+def two_factor_disabled(user):
+    return user[two_fa] == 'False'
+
+# Define the main function
+def main():
+    # Get the command-line arguments
+    try:
+        action = sys.argv[1]
+        rule = sys.argv[2]
+    except IndexError:
+        print('Missing argument(s). Usage: python main.py <True/False> <utilization/2fa>')
+        return
+
+    # Check if the command-line arguments are valid
+    if action not in ('True', 'False') or rule not in ('utilization', '2fa'):
+        print('Invalid argument(s). Usage: python main.py <True/False> <utilization/2fa>')
+        return
+
+    # Check if the users database file exists
+    if not os.path.isfile(users_database):
+        print(f"File {users_database} does not exist.")
+        sys.exit()
+
+      # Read the users database into a list of dictionaries
+    with open(users_database) as user_db:
+        reader = csv.DictReader(user_db)
+        users = [user for user in reader]
+
+        # Remove users based on the selected rule
+        if action == 'True':
+            # Filter out users based on rule
+            if rule == 'utilization':
+                filtered_users = []
+                for user in users:
+                    if utilization_rule(user):
+                        filtered_users.append(user)
+                output = f'Users with utilization more than 50% removed from {os.path.abspath(users_database)}'
+
+            elif rule == '2fa':
+                filtered_users = []
+                for user in users:
+                    if two_factor_disabled(user):
+                        filtered_users.append(user)
+                output = f'Users with two factor authentication enabled removed from {os.path.abspath(users_database)}'
+
+            # Write the filtered users to the users database file
+            with open(users_database, 'w', newline='') as user_db:
+                writer = csv.DictWriter(user_db, fieldnames=reader.fieldnames)
+                writer.writeheader()
+                writer.writerows(filtered_users)
+
+            print(output)
+        else:
+            # Print the names of users based on rule
+            if rule == 'utilization':
+                print('Users with utilization less than 50%: ')
+                for user in users:
+                    if utilization_rule(user):
+                        print(user['emp_name'])
+
+            elif rule == '2fa':
+                filtered_users = []
+                for user in users:
+                    if two_factor_disabled(user):
+                        filtered_users.append(user)
+                print('Users with two factor authentication disabled: ')
+                for user in filtered_users:
+                    print(user['emp_name'])
+if __name__ == '__main__':
+    main()
